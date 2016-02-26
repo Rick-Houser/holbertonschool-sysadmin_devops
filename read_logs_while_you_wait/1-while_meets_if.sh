0d93686c565a2a830fc3e33f6f6a03cdbf544683
Rick Houser <21@p6-4.hbtn.io>
2016-02-26 13:54:53 -0800

    add script to parse given file and output only desired content

---
+++ read_logs_while_you_wait/1-while_meets_if.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# variable set to input file
+file="$1"
+
+# loop through input file's content printing each line
+while read line;
+do
+    echo $line | grep HEAD
+done < $file # exit when reaching end of file  
