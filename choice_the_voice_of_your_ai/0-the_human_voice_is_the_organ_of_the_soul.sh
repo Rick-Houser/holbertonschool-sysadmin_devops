1977cfd4c9d7f56e3721742afa03cb1aaef61254
FrackenToaster <frakentoaster@gmail.com>
2016-02-28 16:13:11 -0800

    create shell script to convert text to audio file and scp to host server

---
+++ choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#####################################################
+# Author: Rick Houser                               #
+# Script: Text2Audio                                #
+#                                                   #
+#	What it does: Shell script takes 3 parameters.    #
+#	First parameter is your text string.              #
+#	Second parameter is your choice of voice:         #
+#		f(female), m(male), x(robot)                    #
+#	Third parameter is the destination IP address.    #
+#	                                                  #
+#	Example:                                          # 
+#	<scriptname> "My text string here" f <IP_ADDRESS> #        
+#####################################################
+
+
+# Takes first word from first parameter and save to FILE
+FILE=$(echo $1 | awk '{ print $1 }')
+
+# Sets voice according to users input
+case $2 in 
+f)
+	VOICE="Karen"
+	;;
+m)
+	VOICE="Alex"
+	;;
+x)
+	VOICE="Bad"
+	;;
+esac
+
+# Saves message($1) with voice type($2) into FILE.mp4
+# where FILE is the first word of the message($1)
+say $1 -v $VOICE -o $FILE.m4a
+
+# Transfer file to server
+IP=$3
+scp -q $FILE.m4a admin@$IP:/usr/share/nginx/html
+
+# Direct user to where audio file can be heard
+echo "$FILE.m4a"
+echo "Listen to the message on http://"$3/$FILE".m4a"
