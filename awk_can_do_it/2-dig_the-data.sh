a070ab303b9db7df2efc055ab0a69ac0563bde33
Rick Houser <21@p6-4.hbtn.io>
2016-03-02 18:34:51 -0800

    add script that parses log file for unique elements (WIP)

--- awk_can_do_it/2-dig_the-data.sh
+++ awk_can_do_it/2-dig_the-data.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+#####################################################
+# Author: Rick Houser                               #
+# Script: LogParser                                 #
+#                                                   #
+# What it does: Script takes a file as parameter.   #
+# Parses log file finding 1st and 9th elements.     #
+# Sorts, counts and outputs unique elements.        #
+# Example:                                          # 
+# <scriptname> /path/to/file.log                    #        
+#####################################################
+
 log=$1
-cat $log | awk '{print $1, $9}'
+cat $log | awk '{print $1, $9}' * | uniq -c | sort 
