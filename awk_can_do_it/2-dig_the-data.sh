67b955c3e8071d4b9a8926211f36862ce38e80f4
Rick Houser <21@p6-4.hbtn.io>
2016-03-04 11:31:16 -0800

    add solution to parse and sort and count occurences within log file handles exception

--- awk_can_do_it/2-dig_the-data.sh
+++ awk_can_do_it/2-dig_the-data.sh
@@ -6,10 +6,11 @@
 #                                                   #
 # What it does: Script takes a file as parameter.   #
 # Parses log file finding 1st and 9th elements.     #
+# If 9th ele isn't status code, print 7th element.  #
 # Sorts, counts and outputs unique elements.        #
 # Example:                                          # 
 # <scriptname> /path/to/file.log                    #        
 #####################################################
 
 log=$1
-cat $log | awk '{print $1, $9}' * | uniq -c | sort 
+awk '{if ($9 == "\"-\"") print $1, $7; else print $1, $9 }' $log | sort | uniq -c | sort
