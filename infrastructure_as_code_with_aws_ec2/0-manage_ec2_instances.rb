ba06742dffac55309c4762687add2b67c5d4c1bb
webrick <11673286+Rick-Houser@users.noreply.github.com>
2016-08-09 04:51:56 -0700

    Returns correct response upon instance creation. TODO: Reduce redundancy, set options help banner and enable verbose mode.

--- infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
@@ -3,22 +3,29 @@ require 'bundler/setup'
 require 'optparse'
 Bundler.require
 
+# Load credentials from yaml file
 creds = YAML.load_file("config.yaml", safe: true)
-instance = Aws::EC2::Client.new(
-		region: 'us-west-2',
-		credentials: Aws::Credentials.new(creds['access_key_id'],
-																			creds['secret_access_key'])
-)
+# instance = Aws::EC2::Client.new(
+# 		region: 'us-west-2',
+# 		credentials: Aws::Credentials.new(creds['access_key_id'],
+# 																			creds['secret_access_key'])
+# )
 
 options = {}
 OptionParser.new do |opt|
   opt.on('-a', '--action ACTION') { |o| options[:action] = o }
 	opt.on('-i', '--instance_id INSTANCE') { |o| options[:instance] = o }
+	opt.on('-h', '--help HELP') { |o| options[:help] = o }
+	opt.on('-v', '--verbose VERBOSE') { |o| options[:verbose] = o }
 end.parse!
 
 case options[:action]
 when "launch" # create the instance (and launch it)
-	ec2 = instance.run_instances(image_id: creds["image_id"],
+	ec2 = Aws::EC2::Resource.new(region: 'us-west-2',
+															credentials: Aws::Credentials.new(creds['access_key_id'],
+																																		creds['secret_access_key']))
+
+	instance = ec2.create_instances({image_id: creds["image_id"],
 														  min_count: 1, # required
 														  max_count: 1, # required
 														  key_name: creds['key_pair'],
@@ -26,21 +33,28 @@ when "launch" # create the instance (and launch it)
 														  instance_type: creds["instance_type"],
 														  placement: {
 																availability_zone: creds["availability-zone"]
-															})
-	puts "Launching machine ..."
-	inst_id = ec2.instances[0].instance_id
-	puts inst_id
-	puts instance.db_instances[0]
+															}
+														})
+	# Wait for the instance to be created, running, and passed status checks
+	check = ec2.client.wait_until(:instance_status_ok, {instance_ids: [instance[0].id]})
+
+	ec2.instances.each do |i|
+		if i.id == instance[0].id
+			puts instance[0].id + "," + i.public_dns_name
+		end
+	end
+	# puts check.reservations[0].instances[0].public_dns_name
 
 when "stop"
 	ec2 = Aws::EC2::Resource.new(region: 'us-west-2',
 															credentials: Aws::Credentials.new(creds['access_key_id'],
 																																creds['secret_access_key']))
 
-	# Stop instance if it exists
+	# Stop instance if it's state isn't already stopped
 	ec2.instances.each do |i|
 		if i.id == options[:instance] && i.state.name != "stopped"
 			i.stop
+			i.wait_until_stopped
 		end
 		# Can also compare status codes.
 		# if i.exists?
@@ -72,12 +86,16 @@ when "start"
 	end
 
 when "terminate"
+	ec2 = Aws::EC2::Resource.new(region: 'us-west-2',
+															credentials: Aws::Credentials.new(creds['access_key_id'],
+																																creds['secret_access_key']))
+
 	ec2.instances.each do |i|
 		if i.id == options[:instance]
 			i.terminate
 		end
 	end
-	
+
 else
 	puts "Invalid action"
 end
